1bd61e755d20d7abfcff23ee2c4b36f77b7c1835
Zachary Yedidia <zyedidia@gmail.com>
2025-12-27 12:30:25 -0500

    Add example with CI workflow

---
+++ example/hello_world.cpp
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "libplatform/libplatform.h"
+#include "v8.h"
+
+int main(int argc, char* argv[]) {
+  // Initialize V8
+  v8::V8::InitializeICUDefaultLocation(argv[0]);
+  v8::V8::InitializeExternalStartupData(argv[0]);
+  std::unique_ptr<v8::Platform> platform = v8::platform::NewDefaultPlatform();
+  v8::V8::InitializePlatform(platform.get());
+  v8::V8::Initialize();
+
+  // Create a new Isolate and make it the current one
+  v8::Isolate::CreateParams create_params;
+  create_params.array_buffer_allocator =
+      v8::ArrayBuffer::Allocator::NewDefaultAllocator();
+  v8::Isolate* isolate = v8::Isolate::New(create_params);
+
+  {
+    v8::Isolate::Scope isolate_scope(isolate);
+    v8::HandleScope handle_scope(isolate);
+
+    // Create a new context
+    v8::Local<v8::Context> context = v8::Context::New(isolate);
+    v8::Context::Scope context_scope(context);
+
+    // Create a string containing the JavaScript source code
+    v8::Local<v8::String> source =
+        v8::String::NewFromUtf8Literal(isolate, "'Hello' + ', World!'");
+
+    // Compile the source code
+    v8::Local<v8::Script> script =
+        v8::Script::Compile(context, source).ToLocalChecked();
+
+    // Run the script to get the result
+    v8::Local<v8::Value> result = script->Run(context).ToLocalChecked();
+
+    // Convert the result to a UTF8 string and print it
+    v8::String::Utf8Value utf8(isolate, result);
+    printf("%s\n", *utf8);
+  }
+
+  // Dispose the isolate and tear down V8
+  isolate->Dispose();
+  delete create_params.array_buffer_allocator;
+  v8::V8::Dispose();
+  v8::V8::DisposePlatform();
+
+  return 0;
+}
